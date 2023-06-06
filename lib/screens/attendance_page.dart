62dca1441d797d7e31125e43bc6f389512ff5ae0
Jaswant Kumar <jaswantk917@gmail.com>
2023-06-06 12:48:19 +0530

    Populated wit demo data

--- lib/screens/attendance_page.dart
+++ lib/screens/attendance_page.dart
@@ -5,12 +5,45 @@ class AttendanceList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Mark the students who are present:',
-        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
-      ),
+    return Column(
+      children: [
+        Card(
+          child: CheckboxListTile(
+            secondary: const Icon(Icons.person),
+            title: const Text('Falna'),
+            subtitle: const Text('Last paid on '),
+            onChanged: (value) {},
+            value: true,
+          ),
+        ),
+        Card(
+          child: CheckboxListTile(
+            secondary: const Icon(Icons.person),
+            title: const Text('Chilna'),
+            subtitle: const Text('Last paid on '),
+            onChanged: (value) {},
+            value: true,
+          ),
+        ),
+        Card(
+          child: CheckboxListTile(
+            secondary: const Icon(Icons.person),
+            title: const Text('Dhimka'),
+            subtitle: const Text('Last paid on '),
+            onChanged: (value) {},
+            value: true,
+          ),
+        ),
+      ],
     );
   }
 }
+
+// const Padding(
+//       padding: EdgeInsets.all(8.0),
+//       child: Text(
+//         'Mark the students who are present:',
+//         style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+//       ),
+//     );
+  
