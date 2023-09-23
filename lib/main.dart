ec25622acc810c13cf10f8817674115d1d4de7b1
Jaswant Kumar <jaswantk917@gmail.com>
2023-09-23 19:03:00 +0530

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,14 @@ import 'package:library_management/screens/students_list.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/attendance_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
